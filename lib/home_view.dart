7f93d4eedf4b6dbc0953c8340f38fbc45b884e11
abhi <manyypal@gmail.ccom>
2023-06-23 14:45:18 +0530

    Home Page

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -31,9 +31,48 @@ class HomeView extends StatelessWidget {
       'userName': 'user5',
     },
   ];
-  final urlImages = [
-    'https://img.freepik.com/premium-vector/character-design-girl-bring-stick_286658-173.jpg?size=626&ext=jpg&ga=GA1.1.647470437.1685963067&semt=robertav1_2_sidr',
-    'https://img.freepik.com/premium-vector/character-design-girl-bring-stick_286658-173.jpg?size=626&ext=jpg&ga=GA1.1.647470437.1685963067&semt=robertav1_2_sidr',
+
+  var postData = [
+    {
+      'imageUrl':
+          'https://img.freepik.com/free-vector/realistic-samurai-illustrated-background_52683-69460.jpg?w=740&t=st=1686137185~exp=1686137785~hmac=390704896744102739b13593a6ee86ac579820b437588272dda37641c152fe9b',
+      'userName': 'Vikram Singh Rathore',
+      "profileImage":
+          "https://img.freepik.com/free-vector/little-blond-boy-anime_18591-77251.jpg?size=626&ext=jpg&ga=GA1.2.647470437.1685963067&semt=robertav1_2_sidr"
+    },
+    {
+      'imageUrl':
+          'https://img.freepik.com/free-vector/little-blond-boy-anime_18591-77251.jpg?size=626&ext=jpg&ga=GA1.2.647470437.1685963067&semt=robertav1_2_sidr',
+      'userName': 'Vivek Kumar',
+      "profileImage":
+          "https://img.freepik.com/premium-vector/character-design-girl-bring-stick_286658-173.jpg?size=626&ext=jpg&ga=GA1.1.647470437.1685963067&semt=robertav1_2_sidr"
+    },
+    {
+      'imageUrl':
+          'https://img.freepik.com/premium-vector/heart-girl-anime-character_603843-485.jpg?size=626&ext=jpg&ga=GA1.2.647470437.1685963067&semt=robertav1_2_sidr',
+      'userName': 'Rohan Singh',
+      "profileImage":
+          "https://img.freepik.com/free-vector/little-blond-boy-anime_18591-77251.jpg?size=626&ext=jpg&ga=GA1.2.647470437.1685963067&semt=robertav1_2_sidr"
+    },
+    {
+      'imageUrl':
+          'https://img.freepik.com/free-photo/girl-with-backpack-sunset-generative-al_169016-28612.jpg?size=338&ext=jpg&ga=GA1.1.647470437.1685963067&semt=robertav1_2_sidr',
+      'userName': 'Rohan Singh',
+      "profileImage":
+          "https://img.freepik.com/premium-vector/character-design-girl-bring-stick_286658-173.jpg?size=626&ext=jpg&ga=GA1.1.647470437.1685963067&semt=robertav1_2_sidr"
+    },
+    {
+      'imageUrl':
+          'https://img.freepik.com/premium-vector/character-design-girl-bring-stick_286658-173.jpg?size=626&ext=jpg&ga=GA1.1.647470437.1685963067&semt=robertav1_2_sidr',
+      'userName': 'Rohan Singh',
+      "profileImage":
+          "https://img.freepik.com/free-vector/little-blond-boy-anime_18591-77251.jpg?size=626&ext=jpg&ga=GA1.2.647470437.1685963067&semt=robertav1_2_sidr"
+    },
+  ];
+
+  var likeImagesList = [
+    'https://img.freepik.com/free-photo/girl-with-backpack-sunset-generative-al_169016-28612.jpg?size=338&ext=jpg&ga=GA1.1.647470437.1685963067&semt=robertav1_2_sidr',
+    'https://img.freepik.com/free-photo/girl-with-backpack-sunset-generative-al_169016-28612.jpg?size=338&ext=jpg&ga=GA1.1.647470437.1685963067&semt=robertav1_2_sidr',
     'https://img.freepik.com/premium-vector/character-design-girl-bring-stick_286658-173.jpg?size=626&ext=jpg&ga=GA1.1.647470437.1685963067&semt=robertav1_2_sidr',
   ];
 
@@ -41,135 +80,189 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.black,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(children: [
-                Text('Instafeed', style: TextStyle(fontSize: 30)),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child:
-                      Icon(Icons.radio_button_unchecked, color: Colors.orange),
-                )
-              ]),
-              Icon(Icons.message)
+        body: SafeArea(
+          child: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                backgroundColor: Colors.black,
+                expandedHeight: 40,
+                floating: true,
+                pinned: false,
+                snap: true,
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(children: [
+                      Text('Instafeed', style: TextStyle(fontSize: 30)),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(Icons.radio_button_unchecked,
+                            color: Colors.orange),
+                      )
+                    ]),
+                    Icon(Icons.message)
+                  ],
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: SizedBox(
+                  height: 120,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      ProfileStoryCard(),
+                      ListView.builder(
+                          itemCount: storyData.length,
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            return StoryCard(
+                              profileImage: storyData[index]['imageUrl']!,
+                              userName: storyData[index]['userName']!,
+                            );
+                          })
+                    ],
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: postData.length,
+                    itemBuilder: (context, index) {
+                      return PostCard(
+                        likeImagesList: likeImagesList,
+                        postData: postData[index],
+                      );
+                    }),
+              ),
             ],
           ),
-        ),
-        body: Column(
-          children: [
-            SizedBox(
-              height: 120,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  ProfileStoryCard(),
-                  ListView.builder(
-                      itemCount: storyData.length,
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        return StoryCard(
-                          profileImage: storyData[index]['imageUrl']!,
-                          userName: storyData[index]['userName']!,
-                        );
-                      })
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(children: [
-                          CircleAvatar(
-                            radius: 35,
-                            backgroundImage: NetworkImage(
-                                "https://img.freepik.com/free-vector/hand-drawn-korean-drawing-style-character-illustration_23-2149623257.jpg?size=338&ext=jpg&ga=GA1.2.647470437.1685963067&semt=robertav1_2_sidr"),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Vikram Singh Rathore",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w500),
-                              ),
-                              Text('15 mins ago',
-                                  style: TextStyle(color: Colors.white38))
-                            ],
-                          )
-                        ]),
-                        Icon(Icons.more_vert)
-                      ]),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.network(
-                      "https://img.freepik.com/free-vector/little-blond-boy-anime_18591-77251.jpg?size=626&ext=jpg&ga=GA1.2.647470437.1685963067&semt=robertav1_2_sidr",
-                      height: 280,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  StackedImagesWidget(
-                    urlImages: urlImages,
-                  )
-                ],
-              ),
-            )
-          ],
         ));
   }
 }
 
-class StackedImagesWidget extends StatelessWidget {
-  final List<String> urlImages;
+class PostCard extends StatelessWidget {
+  const PostCard({
+    super.key,
+    required this.likeImagesList,
+    required this.postData,
+  });
 
-  const StackedImagesWidget({
-    Key? key,
-    required this.urlImages,
-  }) : super(key: key);
+  final List<String> likeImagesList;
+  final dynamic postData;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 35,
-      child: Stack(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          for (int index = 0; index < urlImages.length; index++)
-            Container(
-              width: 35,
-              height: 35,
-              margin: EdgeInsets.only(
-                left: 26.0 * index,
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Row(children: [
+              CircleAvatar(
+                radius: 35,
+                backgroundImage: NetworkImage(postData['profileImage']),
               ),
-              child: ClipOval(
-                child: Container(
-                  padding: EdgeInsets.all(1),
-                  color: Colors.white,
-                  child: ClipOval(
-                    child: Image.network(
-                      urlImages[index],
-                      fit: BoxFit.cover,
-                    ),
+              SizedBox(
+                width: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    postData['userName'],
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
                   ),
-                ),
-              ),
+                  Text('15 mins ago', style: TextStyle(color: Colors.white38))
+                ],
+              )
+            ]),
+            Icon(Icons.more_vert)
+          ]),
+          SizedBox(
+            height: 15,
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.network(
+              postData['imageUrl'],
+              height: 280,
+              width: double.infinity,
+              fit: BoxFit.cover,
             ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  LikesImages(
+                    likeImages: likeImagesList,
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Text("15 Likes",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Icon(Icons.favorite_border_outlined),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Icon(Icons.message_outlined),
+                ],
+              ),
+              Icon(Icons.bookmark_border_outlined),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "View All 48 comments",
+            style: TextStyle(color: Colors.grey),
+          )
         ],
       ),
+    );
+  }
+}
+
+class LikesImages extends StatelessWidget {
+  const LikesImages({
+    super.key,
+    required this.likeImages,
+  });
+  final List<String> likeImages;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        for (int index = 0; index < likeImages.length; index++)
+          Container(
+            width: 35,
+            height: 35,
+            margin: EdgeInsets.only(left: 26.0 * index),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(width: 1, color: Colors.white)),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(likeImages[index]),
+            ),
+          ),
+      ],
     );
   }
 }
